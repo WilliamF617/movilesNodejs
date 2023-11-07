c70a04a6427ecec3ec6e27b156b42adb2cf353bf
WilliamF617 <william_ferney_617@hotmail.com>
2023-11-07 14:19:16 -0500

    configuracion de la base

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import { createPool } from 'mysql2/promise'
-
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } from './config.js'
 const pool = createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database: 'personaid'
+    host: DB_HOST, //ya
+    user: DB_USER,//ya
+    password: DB_PASSWORD,//ya
+    port: DB_PORT,//ya
+    database: DB_NAME //ya
 })
 
 export { pool }
